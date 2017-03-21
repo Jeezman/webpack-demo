0047939f77137557e7133c0418cb2213c014080e
Tobi <tobiadeyemi91@gmail.com>
2017-03-21 11:55:41 +0100

    initial commit

---
+++ webpack.parts.js
@@ -0,0 +1,42 @@
+exports.devServer = ({ host, port } = {}) => ({
+    devServer: {
+        historyApiFallback: true,
+        stats: 'errors-only',
+        host,
+        port,
+        overlay: {
+            errors: true,
+            warnings: true,
+        },
+    },
+});
+
+exports.lintJavascript =({ include, exclude, options }) => ({
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include,
+                exclude,
+                enforce: 'pre',
+
+                loader: 'eslint-loader',
+                options,
+            },
+        ],
+    },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                include,
+                exclude,
+
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+});
