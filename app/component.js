ddc098fe256236ba485ff9998de5a94e854548c0
Tobi <tobiadeyemi91@gmail.com>
2017-03-21 16:17:07 +0100

    added pure-css module

--- app/component.js
+++ app/component.js
@@ -1,6 +1,7 @@
 export default (text = 'Hello world People') => {
     const element = document.createElement('div');
 
+    element.className = 'pure-button';
     element.innerHTML = text;
 
     return element;
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 import component from './component';
 import './main.css';
 import './style.scss';
+import 'purecss';
 
 document.body.appendChild(component());
