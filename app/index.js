9dec6b5005aca396e9a27582cfb2a4cbad51ef30
Tobi <tobiadeyemi91@gmail.com>
2017-03-24 17:07:05 +0100

    demonstrate tree shaking

--- app/index.js
+++ app/index.js
@@ -4,5 +4,8 @@ import './style.scss';
 import 'purecss';
 import 'font-awesome/css/font-awesome.css';
 import 'react';
+import { bake } from './shake';
+
+bake();
 
 document.body.appendChild(component());
