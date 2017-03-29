8a34e9de3253ac91f310ff7ca0d0c8af69bec8c6
Tobi <tobiadeyemi91@gmail.com>
2017-03-29 12:45:48 +0100

    setting up a worker added worker file and instatiated worker

--- app/component.js
+++ app/component.js
@@ -1,15 +1,33 @@
-export default (text = 'Hello world People') => {
-    const element = document.createElement('div');
+import Worker from 'worker-loader!./worker';
 
-    element.className = 'fa fa-hand-spock-o fa-lg pure-button';
-    element.innerHTML = text;
-    element.onclick = () => {
-        import('./lazy').then((lazy) => {
-            element.textContent = lazy.default;
-        }).catch((err) => {
-            console.error(err);
-        });
-    };
+export default () => {
+// export default (text = 'Hello world People') => {
+    // const element = document.createElement('div');
+
+    // element.className = 'fa fa-hand-spock-o fa-lg pure-button';
+    // element.innerHTML = text;
+    // element.onclick = () => {
+    //     import('./lazy').then((lazy) => {
+    //         element.textContent = lazy.default;
+    //     }).catch((err) => {
+    //         console.error(err);
+    //     });
+    // };
+
+    const element = document.createElement('h1');
+    const worker = new Worker();
+    const state = { text: 'foo' };
+
+    worker.addEventListener(
+        'message',
+        ({ data: { text } }) => {
+            state.text = text;
+            element.innerHTML = text;
+        }
+    );
+
+    element.innerHTML = state.text;
+    element.onclick = () => worker.postMessage({ text: state.text });
 
     return element;
 };
